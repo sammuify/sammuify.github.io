5c32254e6e3c4ca8d233b7f599df1d125b99139c
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-05-09 19:07:44 +0530

    printing tables

---
+++ js/tables.js
@@ -0,0 +1,16 @@
+
+var a = 20 ;
+
+function printTableUpto20(a) {
+    console.log('TABLE OF %d', a);
+    for (var i=1; i<= 10; i++) {
+        console.log("%d X %d = %d", i, a, i*a);
+    }
+    console.log();
+    console.log();
+}
+
+for (var i=1; i<=a; i++) {
+    printTableUpto20(i);
+}
+console.log('LOL');
