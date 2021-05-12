940dc6c0e23713e1c77d3d94b2a4b416cad7ad3d
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-05-12 15:23:22 +0530

    day 5 version one

---
+++ js/day5.js
@@ -0,0 +1,5 @@
+console.log(2 + 3);
+console.log("Sammu is tired");
+document.getElementById("first").innerHTML = 2+3;
+document.getElementById("second").innerHTML += "Sammu is yawning!!";
+document.getElementById
