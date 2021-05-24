ecd83276986b56b8daab836413ddb23594eb71b1
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-05-24 15:09:29 +0530

    working canvas

--- draw/js/draw.js
+++ draw/js/draw.js
@@ -36,19 +36,47 @@ function currentSlide(n) {
 
 document.onkeydown = function (event) {
     switch (event.key) {
-       case "ArrowLeft":
-          plusSlides(-1);
-          break;
-       case "ArrowUp":
-          break;
-       case "ArrowRight":
-          plusSlides(1);
-          break;
-       case "ArrowDown":
-          break;
+        case "ArrowLeft":
+            plusSlides(-1);
+            break;
+        case "ArrowUp":
+            break;
+        case "ArrowRight":
+            plusSlides(1);
+            break;
+        case "ArrowDown":
+            break;
     }
 }
 
- function showCanvas() {
+var canvas = document.getElementById('canvas'); 
+var g = canvas.getContext("2d");
+g.strokeStyle = "red";
+g.lineJoin = "round";
+g.lineWidth = 3;
+g.filter = "blur(1px)";
+g.lineCap = 'round';
 
- }
+const
+relPos = pt => [pt.pageX - canvas.offsetLeft, pt.pageY - canvas.offsetTop],
+drawStart = pt => { with(g) { beginPath(); moveTo.apply(g, pt); stroke(); }},
+drawMove = pt => { with(g) { lineTo.apply(g, pt); stroke(); }},
+
+pointerDown = e => drawStart(relPos(e.touches ? e.touches[0] : e)),
+pointerMove = e => drawMove(relPos(e.touches ? e.touches[0] : e)),
+
+mouseDraw = method => e => {
+    if(method=="add") pointerDown(e);
+    canvas[method+"EventListener"]('mousemove', pointerMove);
+    canvas[method+"EventListener"]('mouseup', g.closePath);
+},
+touchDraw = method => e => {
+    if(method=="add") pointerDown(e);
+    canvas[method+"EventListener"]('touchmove', pointerMove);
+    canvas[method+"EventListener"]('touchend', g.closePath);
+}
+
+canvas.addEventListener('mousedown', mouseDraw("add"));
+canvas.addEventListener('touchstart', touchDraw("add"));
+canvas.addEventListener('mouseup', mouseDraw("remove"));
+canvas.addEventListener('touchend', touchDraw("remove"));
