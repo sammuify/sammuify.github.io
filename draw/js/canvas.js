a39871cd3abcf3db19354912fa4a2d3be6e7e6a2
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-05-25 16:07:05 +0530

    added image hover text

--- draw/js/canvas.js
+++ draw/js/canvas.js
@@ -52,11 +52,11 @@ function goBack() {
 }
 
 function showResults() {
-var selectedImage = window.localStorage.getItem("selectedImage");
-document.getElementById("selectedImage").src = selectedImage;
-document.getElementById("canvasImage").src = document.getElementById("canvas").toDataURL();
+    var selectedImage = window.localStorage.getItem("selectedImage");
+    document.getElementById("selectedImage").src = selectedImage;
+    document.getElementById("canvasImage").src = document.getElementById("canvas").toDataURL();
 
-document.getElementsByClassName("canvas-container")[0].style.display = "none";
-document.getElementsByClassName("results")[0].style.display = "block";
+    document.getElementsByClassName("canvas-container")[0].style.display = "none";
+    document.getElementsByClassName("results")[0].style.display = "block";
 
-}  
+}
