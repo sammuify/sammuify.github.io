598eed159ce67b8758ea55bdce7aab671191dd1d
Rajani Karuturi <rajanikaruturi@gmail.com>
2021-05-28 15:37:29 +0530

    USA states done

---
+++ draw/us-states/js/canvas.js
@@ -0,0 +1,66 @@
+var canvas = document.getElementById('canvas');
+canvas.addEventListener("mousedown", startPosition);
+canvas.addEventListener("mouseup", finishPosition);
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("touchstart", startPosition, false);
+canvas.addEventListener("touchend", finishPosition, false);
+canvas.addEventListener("touchmove", draw, false);
+
+canvas.height = window.innerHeight - 100;
+canvas.width = window.innerWidth - 100;
+
+var ctx = canvas.getContext("2d");
+ctx.lineWidth = 2;
+ctx.lineCap = "round";
+ctx.strokeStyle = "black";
+
+var painting = false;
+
+function startPosition(e) {
+    painting = true;
+    draw(e);
+}
+
+function finishPosition(e) {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+
+    var x, y;
+    if (e.type.startsWith("touch")) {
+        var touch = e.touches[0] || e.changedTouches[0];
+        x = touch.pageX;
+        y = touch.pageY;
+    } else {
+        x = e.clientX;
+        y = e.clientY;
+    }
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    // ctx.beginPath();
+    // ctx.moveTo(e.pageX, e.pageY);
+}
+
+document.body.addEventListener('touchmove', function (event) {
+    event.preventDefault();
+ }, false);
+
+function goBack() {
+    window.history.back();
+}
+
+function showResults() {
+    var selectedImage = window.localStorage.getItem("selectedImage");
+    document.getElementById("selectedImage").src = selectedImage;
+    document.getElementById("canvasImage").src = document.getElementById("canvas").toDataURL();
+
+    document.getElementsByClassName("canvas-container")[0].style.display = "none";
+    document.getElementsByClassName("results")[0].style.display = "block";
+
+}
---
+++ draw/us-states/js/index.js
@@ -0,0 +1,55 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("slide");
+    var thumbnails = document.getElementsByClassName("cursor");
+    var captionText = document.getElementsByClassName("caption");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+
+    if (n <= 0) {
+        slideIndex = slides.length;
+    }
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (var i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].className = thumbnails[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    thumbnails[slideIndex - 1].className += " active";
+    captionText[0].innerHTML = thumbnails[slideIndex - 1].alt.toUpperCase();
+    window.localStorage.setItem("selectedImage", slides[slideIndex -1].firstElementChild.src);
+}
+
+function plusSlides(n) {
+    slideIndex = slideIndex + n;
+    showSlides(slideIndex);
+}
+
+function currentSlide(n) {
+    slideIndex = n;
+    showSlides(slideIndex);
+}
+
+document.onkeydown = function (event) {
+    switch (event.key) {
+        case "ArrowLeft":
+            plusSlides(-1);
+            break;
+        case "ArrowUp":
+            break;
+        case "ArrowRight":
+            plusSlides(1);
+            break;
+        case "ArrowDown":
+            break;
+    }
+}
+
+function showCanvas() {
+    window.location.href = "canvas.html";
+}
